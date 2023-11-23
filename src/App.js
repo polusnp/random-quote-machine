63188c55d6b9e1896372ef71b580379f3eeb2e0b
Pavlo Nahurnyi <polusnp@gmail.com>
2023-11-23 02:27:12 +0200

    random-quote-machine-V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import quotes from "./assets/quotes.json"
+import { FaTwitter, FaQuoteLeft } from "react-icons/fa"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRandomQuote = () => {
+  return quotes[Math.floor(Math.random() * quotes.length)]
 }
 
-export default App;
+const getRandomColor = () => {
+  const red = Math.floor(Math.random() * 255)
+  const green = Math.floor(Math.random() * 255)
+  const blue = Math.floor(Math.random() * 255)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const transition = "all 2s"
+
+function App() {
+  const [quote, setQuote] = useState(getRandomQuote())
+  const [color, setColor] = useState(getRandomColor())
+
+  const changeQuoteHandler = () => {
+    setQuote(getRandomQuote())
+    setColor(getRandomColor())
+  }
+
+  return (
+    <div className="background" style={{ backgroundColor: color, transition }}>
+      <div id="quote-box">
+        <div className="quote-content" style={{ color: color, transition }}>
+          <h2 id="text">
+            <FaQuoteLeft size="25" style={{ marginRight: "10px" }} />
+            {quote.quote}
+          </h2>
+          <h4 id="author">- {quote.author}</h4>
+        </div>
+        <div className="buttons">
+          <a
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
+            id="tweet-quote"
+            target="_blank"
+            rel="noreferrer"
+            title="Tweet this quote!"
+            style={{ backgroundColor: color, marginRight: "10px", transition }}
+          >
+            <FaTwitter
+              color="white"
+              style={{ backgroundColor: color, transition }}
+            />
+          </a>
+          <button
+            id="new-quote"
+            onClick={changeQuoteHandler}
+            style={{ backgroundColor: color, transition }}
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
